fb0580f277d889b6dcbdf1ee253cae31653981c5
Bhumesh Domala <bhumesh@sortlyapp.com>
2023-06-13 00:59:18 +0530

    Installed Chakra-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { InputGroup, Stack, Input, InputLeftElement } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Tigerhall Content</h1>
+      <Stack spacing={4}>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <span>Icon here</span>
+          </InputLeftElement>
+          <Input type="tel" placeholder="Phone number" />
+        </InputGroup>
+      </Stack>
     </>
   );
 }
